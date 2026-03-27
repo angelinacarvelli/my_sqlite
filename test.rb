4e37dbb16b75c16836511b8d48e4325466b1586f
Angelina carvelli <angelinacarvellipro@gmail.com>
2026-03-27 11:10:47 +0100

    Update test.rb

--- test.rb
+++ test.rb
@@ -6,17 +6,15 @@ File.delete(db_name) if File.exist?(db_name) # On repart de zéro
 db = SQLite3::Database.new db_name
 db.execute("CREATE TABLE users (name TEXT, age INTEGER);")
 db.close
-
+# can change values here.
 puts " TEST 1 : INSERT "
-MySqliteRequest.new.insert("users").values({"name" => "Alice", "age" => 20}).run
-MySqliteRequest.new.insert("users").values({"name" => "Bob", "age" => 25}).run
 MySqliteRequest.new.insert("users").values({"name" => "Charlie", "age" => 30}).run
 
 puts "Contenu actuel :"
 puts MySqliteRequest.new.from("users").select("*").run
 
 puts "\n TEST 2 : UPDATE (Bob -> Robert) "
-MySqliteRequest.new.update("users").set({"name" => "Robert"}).where("name", "Bob").run
+MySqliteRequest.new.update("users").set({"name" => "ANAEE"}).where("name", "Bob").run
 puts MySqliteRequest.new.from("users").select("*").where("name", "Robert").run
 
 puts "\n TEST 3 : DELETE (Alice) "
